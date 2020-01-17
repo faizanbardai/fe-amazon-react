e349560689dfb5d5f038a207389e29159ad7c0ce
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-01-17 09:09:46 +0100

    first component

---
+++ src/Components/MainComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Products from "./Products";
+
+export default class MainComponent extends Component {
+  render() {
+    return (
+      <div>
+        Hello
+        <Products />
+      </div>
+    );
+  }
+}
